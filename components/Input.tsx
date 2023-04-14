770d5f2aa91aff0c951629a5de7416dbea06b78b
Giovanni Almeida <69661645+GiovanniBot@users.noreply.github.com>
2023-04-14 13:57:18 -0300

    add: auth screen completed

--- components/Input.tsx
+++ components/Input.tsx
@@ -48,7 +48,7 @@ const Input: React.FC<InputProps> = ({
                         transform
                         -translate-y-3
                         scale-75
-                        top-3
+                        top-4
                         z-10
                         origin-[0]
                         left-6
--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,34 +1,42 @@
 import Input from "@/components/Input";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 const Auth = () => {
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
 
+  const [variant, setVariant] = useState('login');
+
+  const toggleVariant = useCallback(() => {
+    setVariant((currentVariant) => (currentVariant === 'login' ? 'register' : 'login'));
+  }, []);
+
   return (
-    <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+    <div className="relative h-full w-full bg-[url('/images/hero.jpeg')] bg-no-repeat bg-center bg-fixed bg-cover">
       <div className="bg-black w-full h-full lg:bg-opacity-50">
-        <nav className="px-12 py-5">
+        <nav className="px-12 py-6">
           <img src="/images/logo.png" alt="Netflix" className="h-12" />
         </nav>
         <div className="flex justify-center">
            <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
             <h2 className="text-white text-4xl mb-8 font-semibold">
-              Sign in
+              {variant === 'login' ? 'Sign in' : 'Register'}
             </h2>
             <div className="flex flex-col gap-4">
-              <Input 
-                id="name"
-                onChange={(e: any) => setName(e.target.value)}
-                value={name}
-                label="Username"
-              />
+              {variant === 'register' && (
+                <Input 
+                  id="name"
+                  onChange={(e: any) => setName(e.target.value)}
+                  value={name}
+                  label="Username"
+                />
+              )}
               <Input 
                 id="email"
                 onChange={(e: any) => setEmail(e.target.value)}
                 value={email}
-                label="Email"
+                label="Email or phone number"
                 type="email"
               />
               <Input 
@@ -39,14 +47,14 @@ const Auth = () => {
                 type="password"
               />
             </div>
-            <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
-              Login
+            <button className="bg-red-600 py-3 text-white font-semibold rounded-md w-full mt-10 hover:bg-red-700 transition">
+              {variant === 'login' ? 'Sign in' : 'Register'}
             </button>
             <p className="text-neutral-500 mt-12">
-              First time using Netflix?
-              <span className="text-white ml-1 hover:underline cursor-pointer">
-                Create an account
-              </span>
+            {variant === 'login' ? 'New to Netflix?' : 'Already have an account?'}&nbsp;
+              <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
+                {variant === 'login' ? 'Sign up now' : 'Sign in now'}
+              </span>.
             </p>
            </div>
         </div>
